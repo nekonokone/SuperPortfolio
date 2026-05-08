a3a5e1ea51dd2ee7c683083d0477421b00e201d5
nekonokone <neko44086@gmail.com>
2026-05-08 11:36:56 +0900

    PinBall

---
+++ PinBallBuild/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "PinBall-2026_0425_PinBall-1.0";
+const contentToCache = [
+    "Build/PinBallBuild.loader.js",
+    "Build/PinBallBuild.framework.js",
+    "Build/PinBallBuild.data",
+    "Build/PinBallBuild.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
